a3d12af7a91c4f71f8cfcd0a821c5476a933d65a
yooss2006 <yooss2006@naver.com>
2024-11-06 09:25:54 +0900

    Feat: 로그인 되어있지 않은 유저가 접근시 로그인 페이지로 리다이렉트

---
+++ src/middleware.ts
@@ -0,0 +1,44 @@
+import { createServerSupabaseClient } from "@/libs/supabase/server";
+import { type NextRequest, NextResponse } from "next/server";
+
+export const applyMiddlewareSupabaseClient = async (request: NextRequest) => {
+  const response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
+
+  const supabase = await createServerSupabaseClient();
+
+  const { pathname } = request.nextUrl;
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (
+    pathname === "/login" ||
+    pathname.startsWith("/_next") ||
+    pathname.startsWith("/static")
+  ) {
+    return response;
+  }
+
+  if (!user) {
+    const loginUrl = new URL("/login", request.url);
+    loginUrl.searchParams.set("redirectedFrom", pathname);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return response;
+};
+
+export async function middleware(request: NextRequest) {
+  return await applyMiddlewareSupabaseClient(request);
+}
+
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
