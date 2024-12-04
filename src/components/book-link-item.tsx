defe83299be7b28ffd5b579a22bc80add9115227
yooss2006 <yooss2006@naver.com>
2024-12-04 20:33:26 +0900

    Feat: 저자 구분이 ^ 특수기호로 된 경우 , 특수문자로 변환

--- src/components/book-link-item.tsx
+++ src/components/book-link-item.tsx
@@ -2,6 +2,7 @@ import { Book } from "@/model/book";
 import Image from "next/image";
 import Link from "next/link";
 import style from "./book-link-item.module.css";
+import { formatAuthor } from "@/utils/string";
 
 export default function BookLinkItem(props: Book) {
   const { title, isbn, image, author } = props;
@@ -10,7 +11,7 @@ export default function BookLinkItem(props: Book) {
       <Image src={image} width={72} height={112} alt={title} />
       <header className={style.header}>
         <h3 className={style.title}>{title}</h3>
-        <p className={style.author}>{author}</p>
+        <p className={style.author}>{formatAuthor(author)}</p>
       </header>
     </Link>
   );
