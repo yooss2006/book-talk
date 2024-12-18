3f112739ed34fb0e6f2cc493a567a904f9f79b94
yooss2006 <yooss2006@naver.com>
2024-12-18 09:26:12 +0900

    Feat: 로고 표시

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,16 @@
 import style from "./header.module.css";
 import HeaderButtonGroup from "./\bheader-button-group";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className={style.header}>
-      <h1>북토크에 오신 것을 환영합니다.</h1>
+      <h1>
+        <Link href="/">
+          <Image src="/logo.svg" width={40} height={40} alt="북토크" />
+        </Link>
+      </h1>
       <HeaderButtonGroup />
     </header>
   );
