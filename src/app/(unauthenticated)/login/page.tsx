4aa7942c05438a3b190ce92abc3579b7bb4aed90
yooss2006 <yooss2006@naver.com>
2024-12-24 11:31:40 +0900

    Style: 로그인 화면 디자인 수정

--- src/app/(unauthenticated)/login/page.tsx
+++ src/app/(unauthenticated)/login/page.tsx
@@ -1,13 +1,23 @@
-import KakaoLoginButton from "@/components/kakao-login-button";
 import { Suspense } from "react";
+import styles from "./page.module.css";
+import SocialLoginButton from "@/components/social-login-button";
 
 export default async function LoginPage() {
   return (
-    <div>
-      <h2>로그인</h2>
-      <Suspense fallback="로딩중...">
-        <KakaoLoginButton />
-      </Suspense>
+    <div className={styles.container}>
+      <div className={styles.loginBox}>
+        <h1 className={styles.title}>로그인</h1>
+        <p className={styles.description}>
+          소셜 계정으로 간편하게 로그인하세요
+        </p>
+
+        <div className={styles.buttonGroup}>
+          <Suspense fallback="로딩중...">
+            <SocialLoginButton type="google" />
+            <SocialLoginButton type="kakao" />
+          </Suspense>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/social-login-button.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { PROD_URL } from "@/config/env";
+import { createBrowserSupabaseClient } from "@/libs/supabase/client";
+import Image from "next/image";
+import { useSearchParams } from "next/navigation";
+
+import styles from "./social-login.module.css";
+
+type Provider = "kakao" | "google";
+
+const SOCIAL_TYPE = {
+  kakao: {
+    icon: "/icon/kakao-logo.svg",
+    name: "kakao",
+  },
+  google: {
+    icon: "/icon/google-logo.svg",
+    name: "google",
+  },
+};
+
+type Props = {
+  type: Provider;
+};
+
+export default function SocialLoginButton({ type }: Props) {
+  const supabase = createBrowserSupabaseClient();
+
+  const searchParams = useSearchParams();
+  const next = searchParams.get("next") ?? "/";
+  const socialTypeObj = SOCIAL_TYPE[type];
+
+  const login = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: socialTypeObj.name as Provider,
+      options: {
+        redirectTo: PROD_URL
+          ? `${PROD_URL}/auth/callback?next=${next}`
+          : `http://localhost:3000/auth/callback?next=${next}`,
+      },
+    });
+
+    if (error) {
+      throw error;
+    }
+  };
+  return (
+    <button onClick={login} className={styles.socialButton}>
+      <Image
+        src={socialTypeObj.icon}
+        alt={socialTypeObj.name}
+        width={20}
+        height={20}
+      />
+      {socialTypeObj.name}로 계속하기
+    </button>
+  );
+}
